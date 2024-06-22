358ef5d642c44a9acc9d3912dd10083f100453c2
ALI Soleimani <john.doe@example.com>
2024-06-22 16:57:08 +0330

    animate

---
+++ app/src/main/java/com/msa/componentcompose/ui/component/animateC/AnimateExample.kt
@@ -0,0 +1,276 @@
+@file:OptIn(ExperimentalAnimationApi::class)
+
+package com.msa.componentcompose.ui.component.animateC
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateInt
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.with
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Button
+import com.msa.componentcompose.ui.theme.ComponentComposeTheme
+
+@Composable
+fun AnimateExample(modifier: Modifier = Modifier) {
+    ComponentComposeTheme {
+        // AnimatedVisibilityExample()
+
+        ///   AnimatedBorderRadius()
+
+
+        //AnimatedTransitionColor()
+        AnimatedTransitionColor2()
+    }
+}
+//        Crossfade(targetState = ) {
+//
+//        }
+
+@Composable
+fun AnimatedTransitionColor2(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        var isVisible by remember {
+            mutableStateOf(false)
+        }
+        var isRound by remember {
+            mutableStateOf(false)
+        }
+
+        Button(onClick = {
+            isVisible = !isVisible
+            isRound = !isRound
+        }) {
+            Text(text = "action")
+
+        }
+
+
+
+        AnimatedContent(
+            targetState = isVisible,
+            modifier = modifier
+                .fillMaxWidth()
+                .weight(1f),
+
+            content = { isVisibel ->
+                if (isVisibel) {
+                    Box(modifier = modifier.background(Color.Red))
+                } else
+                    Box(modifier = modifier.background(Color.Green))
+            },
+            transitionSpec = {
+                //   fadeIn() with fadeOut()
+                slideInHorizontally(
+                    initialOffsetX = {
+                        if(isVisible) it else -it
+                    }
+                ) with slideOutHorizontally(
+                    targetOffsetX = {if(isVisible) -it else it}
+                )
+            }
+        )
+
+
+    }
+}
+
+@Composable
+fun AnimatedTransitionColor(modifier: Modifier = Modifier) {
+    ComponentComposeTheme {
+        Column(
+            modifier = modifier.fillMaxSize()
+        ) {
+            var isVisible by remember {
+                mutableStateOf(false)
+            }
+            var isRound by remember {
+                mutableStateOf(false)
+            }
+
+            Button(onClick = {
+                isVisible = !isVisible
+                isRound = !isRound
+            }) {
+                Text(text = "action")
+
+            }
+            val transition = rememberInfiniteTransition()
+            val color by transition.animateColor(
+                initialValue = Color.Red,
+                targetValue = Color.Green,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(2000),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
+            Box(
+                modifier = modifier
+                    .padding(vertical = 15.dp, horizontal = 15.dp)
+                    .size(200.dp)
+                    .background(color)
+            ) {
+
+            }
+
+        }
+    }
+
+
+}
+
+@Composable
+fun AnimatedBorderRadius(modifier: Modifier = Modifier) {
+    ComponentComposeTheme {
+        Column(
+            modifier = modifier.fillMaxSize()
+        ) {
+            var isVisible by remember {
+                mutableStateOf(false)
+            }
+            var isRound by remember {
+                mutableStateOf(false)
+            }
+
+            Button(onClick = {
+                isVisible = !isVisible
+                isRound = !isRound
+            }) {
+                Text(text = "action")
+
+            }
+
+            val transition = updateTransition(
+                targetState = isRound,
+                label = null
+            )
+
+            //  val borderRadius by animateIntAsState(
+//                targetValue = if (isRound) 40 else 20,
+
+//                animationSpec = tween(
+//                    durationMillis = 3000,
+//                    delayMillis = 500,
+////                   easing = LinearEasing
+//                )
+
+///               animationSpec = spring(
+//                   dampingRatio = Spring.DampingRatioHighBouncy,
+//                   stiffness = Spring.StiffnessVeryLow
+//              )
+
+//            )
+
+
+            val borderRadius by transition.animateInt(
+                transitionSpec = { tween(2000) },
+                label = "borderRadius",
+                targetValueByState = { isRound ->
+                    if (isRound) 100 else 0
+                }
+            )
+            val color by transition.animateColor(
+                transitionSpec = { tween(1000) },
+                label = "color",
+                targetValueByState = { isRound ->
+                    if (isRound) Color.Green else Color.Red
+                }
+            )
+
+
+            Box(
+                modifier = modifier
+                    .padding(vertical = 15.dp, horizontal = 15.dp)
+                    .size(200.dp)
+                    .clip(RoundedCornerShape(borderRadius))
+                    .background(color)
+            ) {
+
+            }
+        }
+    }
+
+}
+
+@Composable
+fun AnimatedVisibilityExample(modifier: Modifier = Modifier) {
+    ComponentComposeTheme {
+        Column(
+            modifier = modifier.fillMaxSize()
+        ) {
+            var isVisible by remember {
+                mutableStateOf(false)
+            }
+
+            Button(onClick = {
+                isVisible = !isVisible
+            }) {
+                Text(text = "action")
+
+            }
+
+            AnimatedVisibility(
+                visible = isVisible,
+                enter = slideInHorizontally() + fadeIn(),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+            ) {
+                Box(modifier = modifier.background(Color.Red)) {
+
+                }
+
+            }
+
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun AnimatedVisibilityExamplePreview() {
+    ///AnimatedVisibilityExample()
+}
+
+@Preview
+@Composable
+private fun AnimateExamplePreview() {
+    AnimateExample()
+}
---
+++ app/src/main/java/com/msa/componentcompose/ui/component/animateC/AnimatedBorderCard.kt
@@ -0,0 +1,238 @@
+package com.msa.componentcompose.ui.component.animateC
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.msa.componentcompose.R
+import com.msa.componentcompose.ui.theme.Typography
+
+
+@Composable
+fun AnimatedBorderCard(
+    modifier: Modifier = Modifier,
+    shape: Shape = RoundedCornerShape(size = 0.dp),
+    borderWidth: Dp = 2.dp,
+    gradient: Brush = Brush.sweepGradient(listOf(Color.Gray, Color.White)),
+    animationDuration: Int = 10000,
+    onCardClick: () -> Unit = {},
+    content: @Composable () -> Unit
+) {
+    val infiniteTransition = rememberInfiniteTransition(label = "Infinite Color Animation")
+    val degrees by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = animationDuration, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ),
+        label = "Infinite Colors"
+    )
+
+    Surface(
+        modifier = modifier
+            .clip(shape)
+            .clickable { onCardClick() },
+        shape = shape
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(borderWidth)
+                .drawWithContent {
+                    rotate(degrees = degrees) {
+                        drawCircle(
+                            brush = gradient,
+                            radius = size.width,
+                            blendMode = BlendMode.SrcIn,
+                        )
+                    }
+                    drawContent()
+                },
+            color = MaterialTheme.colorScheme.surface,
+            shape = shape
+        ) {
+            content()
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AnimatedBorderCardPreview() {
+    AnimatedBorderCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp)
+        ,
+        shape = RoundedCornerShape(8.dp),
+        gradient = Brush.sweepGradient(listOf(Color.Magenta,Color.Cyan)),
+        onCardClick = {
+
+        }
+    ){
+
+        Column(
+            modifier = Modifier.padding(24.dp)
+        ) {
+            Text(
+                text = "سلام ایران",
+                style = Typography.labelMedium
+            )
+            Text(
+                text = "Hi Iran",
+                style = Typography.labelMedium
+
+            )
+            Text(
+                text = "persian",
+                style = Typography.labelMedium
+            )
+            Image(
+                painter = painterResource(id = R.drawable.flag_iran),
+                contentDescription = "icon"
+            )
+        }
+    }
+}
+
+
+@Composable
+fun AnimatedBorderCardRadar(
+    modifier: Modifier = Modifier,
+    shape: Shape = RoundedCornerShape(size = 0.dp),
+    borderWidth: Dp = 2.dp,
+    gradient: Brush = Brush.sweepGradient(listOf(Color.Gray, Color.White)),
+    animationDuration: Int = 10000,
+    onCardClick: () -> Unit = {},
+    content: @Composable () -> Unit
+) {
+    val infiniteTransition = rememberInfiniteTransition(label = "Infinite Color Animation")
+    val degrees by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = animationDuration, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ),
+        label = "Infinite Colors"
+    )
+
+    Surface(
+        modifier = modifier
+            .clip(shape)
+            .clickable { onCardClick() },
+        shape = shape
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(borderWidth)
+                .drawWithContent {
+                    rotate(degrees = degrees) {
+                        drawCircle(
+                            brush = gradient,
+                            radius = size.width,
+                            blendMode = BlendMode.SrcIn,
+                        )
+                    }
+                    //drawContent()
+                },
+            color = MaterialTheme.colorScheme.surface,
+            shape = shape
+        ) {
+            content()
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun AnimatedBorderCardRadarPreciew() {
+    AnimatedBorderCardRadar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp)
+        ,
+        shape = RoundedCornerShape(8.dp),
+        gradient = Brush.sweepGradient(listOf(Color.Magenta,Color.Cyan)),
+        onCardClick = {
+
+        }
+    ){
+
+        Column(
+            modifier = Modifier.padding(24.dp)
+        ) {
+            Text(
+                text = "سلام ایران",
+                style = Typography.labelMedium
+            )
+            Text(
+                text = "Hi Iran",
+                style = Typography.labelMedium
+
+            )
+            Text(
+                text = "persian",
+                style = Typography.labelMedium
+            )
+            Image(
+                painter = painterResource(id = R.drawable.flag_iran),
+                contentDescription = "icon"
+            )
+        }
+    }
+}
